ca6bd57efde2ee3dccd494a1951fa9bc70bb955f
h4dri1 <hadrien.vuillaume@gmail.com>
2022-01-21 00:08:40 +0100

    chore(back):add swagger doc

--- back/app/router.js
+++ back/app/router.js
@@ -2,11 +2,21 @@ const  {Router} = require('express');
 
 const router = Router();
 
-const userController = require('./controllers/userController')
+const userController = require('./controllers/userController');
 
-router.get('/hello', (req, res) => {
-    res.send('hello world !');
-});
+/**
+* @typedef {Object} UserPost
+* @property {string} email
+* @property {string} password
+*/
+
+/**
+ * POST /v1/login
+ * @summary Login
+ * @param {UserPost} request.body.required User Object from login
+ * @returns {object} 200 - User connected
+ * @returns {object} 500 - An error message
+ */
 
 router.post('/login', userController.validLogin);
 
--- back/index.js
+++ back/index.js
@@ -10,6 +10,35 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const expressJSDocSwagger = require('express-jsdoc-swagger');
+
+const options = {
+    info: {
+      version: '1.0.0',
+      title: 'Mycryptofolio-back',
+      description: 'A crypto monitoring REST API',
+      license: {
+        name: 'MIT',
+      },
+    },
+    security: {
+      BasicAuth: {
+        type: 'http',
+        scheme: 'basic',
+      },
+    },
+    baseDir: __dirname,
+    filesPattern: './**/*.js',
+    swaggerUIPath: '/api-docs',
+    exposeSwaggerUI: true,
+    exposeApiDocs: false,
+    apiDocsPath: '/v3/api-docs',
+    notRequiredAsNullable: false,
+    swaggerUiOptions: {},
+};
+  
+expressJSDocSwagger(app)(options);
+
 app.use(cors());
 
 app.use(express.json());
