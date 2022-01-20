f6a85cf7bdb00762741800966e7d0e5be981a9d3
Augustin Holguin <augustin.holguin@gmail.com>
2022-01-20 16:35:32 +0100

    feat(cryptoList): basic elements of table layed out + test with mock data

--- front/src/components/CryptoList/index.js
+++ front/src/components/CryptoList/index.js
@@ -1,5 +1,39 @@
+import { Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
+
+import cryptos from './cryptos';
+
+// useStyles to set a min width on Table class for mobile view
+
 const CryptoList = () => (
-    <p>Crypto List</p>
+    <>
+        <p>Crypto List</p>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>#</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>24h %</TableCell>
+                    <TableCell>Market Cap</TableCell>
+                    <TableCell>Volume 24h</TableCell>
+                    <TableCell>Circulating supply</TableCell>
+                    <TableCell>Price</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {cryptos.map((crypto) => (
+                    <TableRow key={crypto.id}>
+                        <TableCell>{crypto.market_cap_rank}</TableCell>
+                        <TableCell>{crypto.name}</TableCell>
+                        <TableCell>{crypto.price_change_percentage_24h}</TableCell>
+                        <TableCell>{crypto.market_cap}</TableCell>
+                        <TableCell>{crypto.total_volume}</TableCell>
+                        <TableCell>{crypto.circulating_supply}</TableCell>
+                        <TableCell>{crypto.current_price}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    </>
 );
 
 export default CryptoList;
