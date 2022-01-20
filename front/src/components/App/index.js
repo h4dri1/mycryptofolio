1c2d626f91c56a103a29cfd79950d6c9dd8705d6
Vincent Mingam <vmingam@gmail.com>
2022-01-20 10:27:55 +0100

    fix(App): clean broken links to old Counter component

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -1,15 +1,10 @@
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
 
-import Counter from '../Counter';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
     <h1>Composant : App</h1>
-    <Counter />
   </div>
 );
 
