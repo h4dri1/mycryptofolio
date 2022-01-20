9623ff1ffa2cf31bcf5600cc61e25079989dd2ee
Vincent Mingam <vmingam@gmail.com>
2022-01-20 15:30:39 +0100

    feat(Login): init a basic Modal component

--- front/src/components/Login/index.js
+++ front/src/components/Login/index.js
@@ -1,5 +1,33 @@
+import { Modal, Button, Box, Typography } from '@mui/material';
+import { useState } from 'react';
+
 export default function Login() {
+  const [open, setOpen] = useState(false)
+  console.log(open)
+  const handleOpen = () => {
+    setOpen(true);
+  }
+  const handleClose = () => {
+    setOpen(false);
+  }
   return (
-    <div className="">Login</div>
+    <>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </>
   );
 }
