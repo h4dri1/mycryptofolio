1d49f8900a348bcd5b7c2fff5729b047dd0d77d9
Augustin Holguin <augustin.holguin@gmail.com>
2022-01-21 17:28:43 +0100

    feat(cryptoList): added some responsiveness

--- front/src/components/CryptoList/index.js
+++ front/src/components/CryptoList/index.js
@@ -9,23 +9,17 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 
-import { makeStyles } from '@mui/styles';
+import { useMediaQuery } from '@mui/material';
 
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { getCryptoList, getMoreCryptos } from 'src/actions/cryptos';
 
-const useStyles = makeStyles({
-    table: {
-        maxWidth: '80vw',
-        margin: 'auto'
-    }
-});
-
 const CryptoList = () => {
-    const classes = useStyles();
+    const mediumScreen = useMediaQuery('(min-width:900px)');
 
     const dispatch = useDispatch();
 
@@ -36,44 +30,47 @@ const CryptoList = () => {
     }, []);
 
     return (
-        <>
-            <Table className={classes.table}>
-                <TableHead>
-                    <TableRow>
-                        <TableCell align="center"><TableSortLabel />#</TableCell>
-                        <TableCell>Name</TableCell>
-                        <TableCell align="right">24h %</TableCell>
-                        <TableCell align="right">Market Cap</TableCell>
-                        <TableCell align="right">Volume 24h</TableCell>
-                        <TableCell align="right">Circulating supply</TableCell>
-                        <TableCell align="center">Price</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {cryptos.map((crypto) => (
-                        <TableRow key={crypto.id} hover>
-                            <TableCell align="center">{crypto.market_cap_rank}</TableCell>
-                            <TableCell>
-                                <Box sx={{ display: 'flex', alignItems: 'center' }}>                             
-                                    <Avatar src={crypto.image} alt={crypto.name} sx={{ mr: 2 }} />                               
-                                    <Link underline="hover" variant="body1" sx={{ mr: 1 }}>{crypto.name}</Link>
-                                    <Typography>{crypto.symbol.toUpperCase()}</Typography>
-                                </Box>
-                            </TableCell>
-                            <TableCell align="right">{crypto.price_change_percentage_24h.toLocaleString()}%</TableCell>
-                            <TableCell align="right">{'$' + crypto.market_cap.toLocaleString()}</TableCell>
-                            <TableCell align="right">{'$' + crypto.total_volume.toLocaleString()}</TableCell>
-                            <TableCell align="right">{'$' + crypto.circulating_supply.toLocaleString()}</TableCell>
-                            <TableCell align="center">{'$' + crypto.current_price.toLocaleString()}</TableCell>
+        <Grid container justifyContent="center" spacing={{xs: 2, sm: 4}}>
+            <Grid item xs={12}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center"><TableSortLabel />#</TableCell>
+                            <TableCell>Name</TableCell>
+                            <TableCell align="right">24h %</TableCell>
+                            <TableCell align="right" sx={{ display: { xs: 'none', sm: 'table-cell' }}}>Market Cap</TableCell>
+                            <TableCell align="right" sx={{ display: { xs: 'none', md: 'table-cell' }}}>Volume 24h</TableCell>
+                            <TableCell align="right"  sx={{ display: { xs: 'none', md: 'table-cell' }}}>Circulating supply</TableCell>
+                            <TableCell align="center">Price</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+                    </TableHead>
+                    <TableBody>
+                        {cryptos.map((crypto) => (
+                            <TableRow key={crypto.id} hover>
+                                <TableCell align="center">{crypto.market_cap_rank}</TableCell>
+                                <TableCell>
+                                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                        <Avatar src={crypto.image} alt={crypto.name} sx={{ mr: 2 }} />
+                                        <Link underline="hover" variant="body1" sx={{ mr: 1}}>{crypto.name}</Link>
+                                        <Typography sx={{display: { xs: 'none', sm: 'block'}}}>{crypto.symbol.toUpperCase()}</Typography>
+                                    </Box>
+                                </TableCell>
+                                <TableCell align="right">{crypto.price_change_percentage_24h.toLocaleString()}%</TableCell>
+                                <TableCell align="right" sx={{ display: { xs: 'none', sm: 'table-cell' }}}>{'$' + crypto.market_cap.toLocaleString()}</TableCell>
+                                <TableCell align="right" sx={{ display: { xs: 'none', md: 'table-cell' }}}>{'$' + crypto.total_volume.toLocaleString()}</TableCell>
+                                <TableCell align="right" sx={{ display: { xs: 'none', md: 'table-cell' }}}>{'$' + crypto.circulating_supply.toLocaleString()}</TableCell>
+                                <TableCell align="center">{'$' + crypto.current_price.toLocaleString()}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </Grid>
 
-            <Button href="" variant="outlined" onClick={() => dispatch(getMoreCryptos())}>Charger plus de cryptos</Button>
-        </>
+            <Grid item>
+                <Button href="" variant="outlined" onClick={() => dispatch(getMoreCryptos())}>Charger plus de cryptos</Button>
+            </Grid>
+        </Grid>
     );
 }
-
 
 export default CryptoList;
