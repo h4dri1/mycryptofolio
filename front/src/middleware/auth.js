0887210a65981bc0ec95bec92dd36f13f5e0132e
Vincent Mingam <vmingam@gmail.com>
2022-01-21 09:47:09 +0100

    feat(Login): update API url

--- front/src/middleware/auth.js
+++ front/src/middleware/auth.js
@@ -5,7 +5,8 @@ const auth = (store) => (next) => (action) => {
   switch (action.type) {
     case LOGIN:
       const state = store.getState();
-      axios.post('http://mycryptofolio.fr:8888/v1/login', {
+      console.log(state)
+      axios.post('https://dev.mycryptofolio.fr/v1/login', {
           email: state.user.email,
           password:state.user.password,
           // email: "test@test.fr",
@@ -15,6 +16,7 @@ const auth = (store) => (next) => (action) => {
      .then((res) => {
        alert(`${res.data}, vous êtes bien connecté`)
      })
+     .catch((err) => console.log(err));
       
       break;
   
