f91cb7ad60aabe32a747d7dfe979b95308bfac3b
Etudiant O'clock <g.tannier@gmail.com>
2022-01-21 11:56:49 +0100

    fix(Logo):resize the logo

--- front/src/components/Navbar/Logo/index.js
+++ front/src/components/Navbar/Logo/index.js
@@ -1,14 +1,17 @@
 import logo from 'src/assets/logo/logo_MyCryptoFolio.png';
+import Avatar from '@mui/material/Avatar';
 
 export default function Logo() {
     return (
         <>
-            <img src={logo} alt="logo MyCryptoFolio"
-                sx={{ width: 500, height: 450 }}
-                variant="quilted"
-                cols={4}
-                rowHeight={121}
+            <Avatar
+                alt="logo MyCryptoFolio"
+                src={logo}
+                sx={{ width: 65, height: 65 }}
+                variant="square"
             />
         </>
     );
 }
+
+{/* <img src={logo} alt="logo MyCryptoFolio" */ }
