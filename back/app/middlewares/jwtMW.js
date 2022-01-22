c92e925e721d322b5fffcdb97ca36db92a3674d3
h4dri1 <hadrien.vuillaume@gmail.com>
2022-01-22 02:44:42 +0100

    fix(back):fix route doc

--- back/app/middlewares/jwtMW.js
+++ back/app/middlewares/jwtMW.js
@@ -15,6 +15,6 @@ module.exports = (req, res, next) => {
         next();
     } catch(error) {
         console.log(error);
-        res.status(401).send(error.name);
+        res.status(401).json(error.name);
     };
 };
