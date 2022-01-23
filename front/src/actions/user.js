7a5eea277b5d6afece0701f43cb4f543ac431ecd
Vincent Mingam <vmingam@gmail.com>
2022-01-23 21:19:11 +0100

    feat(Login): get user infos from token and store them

--- front/src/actions/user.js
+++ front/src/actions/user.js
@@ -1,6 +1,6 @@
 export const CHANGE_FIELD = 'CHANGE_FIELD';
 export const LOGIN = 'LOGIN';
-// export const SAVE_USER = 'SAVE_USER';
+export const SAVE_USER = 'SAVE_USER';
 
 export const changeField = (key, value) => ({
   type: CHANGE_FIELD,
@@ -14,8 +14,8 @@ export const login = () => ({
   type: LOGIN,
 });
 
-// export const saveUser = (user) => ({
-//   type: SAVE_USER,
-//   payload: { ...user }
-// })
+export const saveUser = (user) => ({
+  type: SAVE_USER,
+  payload: { ...user }
+})
 
