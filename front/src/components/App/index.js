37740545f47e545e255b9f60c520b8d7e82ea73e
Vincent Mingam <vmingam@gmail.com>
2022-01-23 22:27:03 +0100

    test(Login): add logout test button and its handler
    To be dropped after integration

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -1,17 +1,31 @@
 // == Import
+import { useDispatch } from 'react-redux';
+import { logout } from '../../actions/user';
 import Home from 'src/pages/Home';
 import Login from 'src/components/Login';
 import Footer from 'src/components/Footer';
+import { Button } from '@mui/material'
 
 const loginDisplay = true;
 
+
 // == Composant
-const App = () => (
-  <div className="app">
-    <Home />
-    {loginDisplay && <Login />}
-  </div>
-);
+const App = () => {
+
+  const dispatch = useDispatch();
+  
+  const handleLogout = () => {
+    dispatch(logout())
+  };
+
+  return (
+    <div className="app">
+      <Home />
+      {loginDisplay && <Login />}
+      <Button onClick={handleLogout} variant='contained' color='error'>Déconnexion</Button>
+    </div>
+  );
+}
 
 // == Export
 export default App;
