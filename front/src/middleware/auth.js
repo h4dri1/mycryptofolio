1bc7d8d21a27fd52f347f37ec472604c7c4f9776
Vincent Mingam <vmingam@gmail.com>
2022-01-23 19:01:48 +0100

    feat(Login): store tokens

--- front/src/middleware/auth.js
+++ front/src/middleware/auth.js
@@ -7,19 +7,23 @@ const auth = (store) => (next) => (action) => {
     case LOGIN:
       const state = store.getState();
       // TODO: ajouter la fonction cleanObject de DOM-Purify pour nettoyer les valeurs des champs
-      axios.post('https://dev.mycryptofolio.fr/v1/login', {
-          email: state.user.email,
-          password:state.user.password,
-          // email: "test@test.fr",
-          // password: "#0clock$0087",
-        }
-      )
+      axios.post('https://dev.mycryptofolio.fr/v1/jwt/login', {
+        email: state.user.email,
+        password:state.user.password,
+        // email: "test@test.fr",
+        // password: "#0clock$0087",
+      })
      .then((res) => {
        if (res.status === 200) {
          // TODO: alert should be superseded by opening AlertMessage component (src/common)
-         alert(`${res.data}, vous êtes bien connecté`)
+         alert(`${res.data.status}, vous êtes bien connecté`)
+         
          // close the Login modal
          store.dispatch(toggleLoginModal())
+         
+         // store tokens
+         localStorage.setItem('refresh', res.data.refreshToken);
+         localStorage.setItem('access', res.headers['authorization']);
        }
      })
      .catch((err) => {
