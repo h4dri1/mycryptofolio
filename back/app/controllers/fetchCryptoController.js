393a5fd1c155a2566d558e658965eff0e604ec29
h4dri1 <hadrien.vuillaume@gmail.com>
2022-01-24 11:53:22 +0100

    build(back):change cryptoprice route add multiple optional params

--- back/app/controllers/fetchCryptoController.js
+++ back/app/controllers/fetchCryptoController.js
@@ -25,7 +25,20 @@ module.exports = {
 
     getOnePrice: async (req, res) => {
         try {
-            const coins = await fetch(`//api.coingecko.com/api/v3/simple/price?ids=${req.params.id}&vs_currencies=${req.params.vs}`);
+            let link = `//api.coingecko.com/api/v3/simple/price?ids=${req.params.id}&vs_currencies=${req.params.vs}`
+            if (req.params.include_market_cap) {
+                link = link + '&include_market_cap=true';
+            }
+            if (req.params.include_24hr_vol) {
+                link = link + '&include_24hr_vol=true';
+            }
+            if (req.params.include_24hr_change) {
+                link = link + '&include_24hr_change=true';
+            }
+            if (req.params.include_last_updated_at) {
+                link = link + '&include_last_updated_at=true';
+            }
+            const coins = await fetch(link);
             const data = await coins.json();
             res.status(200).json(data);
         } catch (error) {
