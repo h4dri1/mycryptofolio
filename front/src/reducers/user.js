f4633b5153aeca7d7d9f45637fbe9a136a126e13
Vincent Mingam <vmingam@gmail.com>
2022-01-24 12:32:55 +0100

    style(Login): minor eslint corrections

--- front/src/reducers/user.js
+++ front/src/reducers/user.js
@@ -2,40 +2,39 @@ import {
   CHANGE_FIELD,
   LOGIN,
   SAVE_USER,
-  LOGOUT
-} from '../actions/user'
+  LOGOUT,
+} from '../actions/user';
 
 export const initialState = {
   logged: false,
   email: '',
   password: '',
   nickname: '',
-  avatar: ''
-}
+  avatar: '',
+};
 
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
-
     case CHANGE_FIELD: {
       return ({
         ...state,
-        [action.payload.key]: action.payload.value
+        [action.payload.key]: action.payload.value,
       });
     }
 
     case LOGIN: {
       return ({
         ...state,
-        password: ''
-      })
+        password: '',
+      });
     }
-  
+
     case SAVE_USER: {
       return ({
         ...state,
         ...action.payload,
         logged: true,
-      })
+      });
     }
 
     case LOGOUT: {
@@ -45,12 +44,12 @@ const reducer = (state = initialState, action = {}) => {
         email: '',
         nickname: '',
         avatar: '',
-      })
+      });
     }
 
     default:
       return state;
   }
-}
+};
 
 export default reducer;
