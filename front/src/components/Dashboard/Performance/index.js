ccd4728916f7399cf11054f29358008c4ccbacb8
Augustin Holguin <augustin.holguin@gmail.com>
2022-01-25 12:56:47 +0100

    feat(portfolio-history): init of layouts of elements

--- front/src/components/Dashboard/Performance/index.js
+++ front/src/components/Dashboard/Performance/index.js
@@ -1,5 +1,38 @@
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+
 const Performance = () => (
-    <p>Performance</p>
+  <Grid container sx={{ height: '100%' }}>
+    <Grid item xs={12} sx={{ height: '15%' }}>
+      <Typography variant="h6">Performance</Typography>
+    </Grid>
+    <Grid item xs={12} sm={6}>
+      <Card>
+        <Typography>Investissement</Typography>
+        <Typography>Valeur actuelle</Typography>
+        <Typography>PnL</Typography>
+      </Card>
+    </Grid>
+    <Grid
+      item
+      xs={12}
+      sm={6}
+      sx={{
+        height: '85%', display: 'flex', justifyContent: 'center', alignItems: 'center',
+      }}
+    >
+      <Box
+        component="span"
+        sx={{
+          height: '200px', width: '200px', borderRadius: '50%', border: 'solid 1px grey',
+          display: 'flex', alignItems: 'center', justifyContent: 'center',
+        }}
+      ><Typography>+50%</Typography>
+      </Box>
+    </Grid>
+  </Grid>
 );
 
 export default Performance;
