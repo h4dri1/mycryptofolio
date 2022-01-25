7b632b823f8820c3ce5e303b8945d4e2da62927b
Vincent Mingam <vmingam@gmail.com>
2022-01-25 14:42:36 +0100

    fix(auth): Fix Clean and refacto to get checkToken effective

--- front/src/services/getNewAccessToken.js
+++ front/src/services/getNewAccessToken.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-export const getNewAccessToken = async (token) => {
-    try {
-      const response = await axios(`https://dev.mycryptofolio.fr/v1/jwt/refresh/${token}`)
-      console.log(response.status, response.data, response.headers.authorization)
-      if (response.status === 200){
-        console.log('New AccessToken')
-        localStorage.setItem('accessToken', response.headers.authorization);
-      }
-    } catch (error) {
-    console.log(error.response)
-    dispatch(logout())
+const getNewAccessToken = async (token) => {
+  try {
+    const response = await axios(`https://dev.mycryptofolio.fr/v1/jwt/refresh/${token}`);
+    console.log(response.status, response.data, response.headers.authorization);
+    if (response.status === 200) {
+      return response.headers.authorization;
     }
-  };
+  }
+  catch (error) {
+    return error;
+  }
+};
+export default getNewAccessToken;
