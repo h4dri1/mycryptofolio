3350f999c2da595f88a9fb01110c0797b917ffc4
Vincent Mingam <vmingam@gmail.com>
2022-01-25 16:21:58 +0100

    wip(Dashboard): Init transaction Box

--- front/src/components/Dashboard/TransactionCreator/index.js
+++ front/src/components/Dashboard/TransactionCreator/index.js
@@ -1,5 +1,37 @@
+import { Box, Grid, Typography } from '@mui/material';
+
 const TransactionCreator = () => (
-    <p>TransactionCreator</p>
+  <Box
+    sx={{
+      display: 'flex', flexDirection: 'column', alignItems: 'center', maxHeight: '50vh', overflowY: 'auto',
+    }}
+  >
+    <Typography
+      variant="h6"
+      component="h2"
+    >
+      Enregistrer une transaction
+    </Typography>
+    <Grid container xs={12}>
+      <Grid item xs={12}> Crypto </Grid>
+    </Grid>
+    <Grid container xs={12}>
+      <Grid item xs={6}> Quantité </Grid>
+      <Grid item xs={6}> Prix </Grid>
+    </Grid>
+    <Grid container xs={12}>
+      <Grid item container xs={6}>
+        <Grid item xs={12}> Date </Grid>
+        <Grid item container xs={12}>
+          <Grid item xs={6}> Ajouter </Grid>
+          <Grid item xs={6}> Annuler </Grid>
+        </Grid>
+      </Grid>
+      <Grid item container xs={6}>
+        <Grid item xs={12}> Valeur: $ 1.23 </Grid>
+      </Grid>
+    </Grid>
+  </Box>
 );
 
 export default TransactionCreator;
