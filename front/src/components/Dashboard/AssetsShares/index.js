8026f326b3f52047b32e8ac7b11246304cb6eaa0
Etudiant O'clock <g.tannier@gmail.com>
2022-01-25 18:43:52 +0100

    feat(AssetsShares):add pie graph + details

--- front/src/components/Dashboard/AssetsShares/index.js
+++ front/src/components/Dashboard/AssetsShares/index.js
@@ -1,22 +1,34 @@
-// const AssetsShares = () => (
-//     <p>AssetsShares</p>
-// );
-
-// export default AssetsShares;
-
 import React from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
+import Container from '@mui/material/Container';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import Divider from '@mui/material/Divider';
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTransactionsHist } from 'src/actions/portfolio';
+
+const options = {
+    plugins: {
+        legend: false,
+    },
+};
 
 const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Asset 1', 'Asset 2'],
     datasets: [
         {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            // label: '% of assets',
+            data: [12, 19],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -33,12 +45,61 @@ const data = {
                 'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)',
             ],
-            borderWidth: 1,
+            borderWidth: 2,
         },
     ],
 }
 
-export default function App() {
-    return <Pie data={data} />;
+export default function AssetsShares() {
+    const dispatch = useDispatch();
+
+    const { transactionsList } = useSelector((state) => state.portfolio);
+
+    useEffect(() => {
+        dispatch(getTransactionsHist());
+    }, []);
+
+    const SumCrypto = {
+
+    }
+
+    return (
+        <>
+            <Container disableGutters>
+                <ListItem>
+                    <ListItemText primary="Répartition de vos actifs" sx={{ textAlign: "center" }} />
+                </ListItem>
+                <Divider sx={{ width: "100%" }}></Divider>
+
+                <Container
+                    sx={{ width: '40%', height: '40%', mt: 1 }}>
+                    <Pie data={data} options={options} />
+                </Container>
+                <Container>
+                    <Table stickyHeader sx={{ maxWidth: '90%' }}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="left">Devise</TableCell>
+                                <TableCell align="center">Quantité</TableCell>
+                                <TableCell align="center">Total $</TableCell>
+                                <TableCell align="center">répartition</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {transactionsList.map((transaction) => (
+                                <TableRow key={transaction.id}>
+                                    <TableCell align="left">{transaction.coinId}</TableCell>
+
+                                    <TableCell align="center">{transaction.quantity}</TableCell>
+
+                                    <TableCell align="right">{transaction.rentability}%</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </Container>
+            </Container>
+        </>
+    )
 }
 
