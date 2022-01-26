f7d52a7409ccd1da1fb8f020b03e326a0511f09a
h4dri1 <hadrien.vuillaume@gmail.com>
2022-01-26 18:08:38 +0100

    clean(back):remove fetchController and add method to cryptoController with usage of fetch service

--- back/app/controllers/index.js
+++ back/app/controllers/index.js
@@ -1,11 +1,9 @@
-const fetchCryptoController = require('./fetchCryptoController');
 const tokenController = require('./tokenController');
 const userController = require('./userController');
 const cryptoController = require('./cryptoController');
 const transactionController = require('./transactionController');
 
 module.exports = {
-    fetchCryptoController,
     tokenController,
     userController,
     cryptoController,
