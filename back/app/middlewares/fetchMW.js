27dde3de3e9b148b87c3d1d35eed3be925d771af
h4dri1 <hadrien.vuillaume@gmail.com>
2022-01-26 15:27:50 +0100

    refactor(back):refacto and clean transactionController

--- back/app/middlewares/fetchMW.js
+++ back/app/middlewares/fetchMW.js
@@ -1,4 +1,5 @@
-const { Transaction } = require('../models')
+const { Transaction } = require('../models');
+const service_fetch = require('../services/fetch');
 
 module.exports = async (req, res, next) => {
     try {
@@ -6,7 +7,13 @@ module.exports = async (req, res, next) => {
         if (!cryptos) {
             return res.status(500).json(error.message, true);
         };
-        res.locals.data = cryptos;
+        const strCryptos = cryptos.map((crypto) => {
+            return crypto['coin_id']
+        });
+        searched_coins = strCryptos.toString();
+        const data = await service_fetch(`//api.coingecko.com/api/v3/simple/price?ids=${searched_coins}&vs_currencies=usd`);
+        res.locals.cryptos = cryptos;
+        res.locals.price = data;
         next();
     } catch (error) {
         console.log(error);
