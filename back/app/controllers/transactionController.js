3eab12ef7d770dc878055bc92214f08ed0549d14
h4dri1 <hadrien.vuillaume@gmail.com>
2022-01-26 17:32:16 +0100

    build(back):finish portfolio route with big portfolio object

--- back/app/controllers/transactionController.js
+++ back/app/controllers/transactionController.js
@@ -1,40 +1,55 @@
 const { Transaction } = require('../models');
-const service_fetch = require('../services/fetch');
 
 module.exports = {
     getPortfolio: async (req, res) => {
-        let portfolio = {};
-        let repartition = {};
-        let objRepartition = {};
-        let sumValue = 0;
-
         try {
-            const transactions = await Transaction.getUserTransaction(req.params.id);
-            if (!transactions) {
+            const cryptos = res.locals.cryptos;
+            const price = res.locals.price
+
+            let portfolio = {};
+            let value = {};
+            let buy = {}
+            let objRepartition = {};
+            let objPerformance = {};
+            let sumValue = 0;
+            let sumBuy = 0;
+
+            const objTransactions = await Transaction.getUserTransaction(req.params.id);
+            if (!objTransactions) {
                 return res.status(500).json(error.message, true);
             };
-            const cryptos = res.locals.cryptos;
-            const data = res.locals.price
-            portfolio.Transactions = transactions;
-            console.log(cryptos);
-            console.log(data);
-            for (const test of cryptos) {
-                repartition[test.coin_id] = test.total * data[test.coin_id].usd
+
+            for (const val of cryptos) {
+                value[val.coin_id] = val.total * price[val.coin_id].usd
+                buy[val.coin_id] =  val.total * val.buy_price
             }
-            console.log(repartition)
-            for (const key in repartition) {
-                sumValue += parseInt(repartition[key])
+
+            for (const key in value) {
+                sumValue += parseInt(value[key])
             }
-            for (const test2 of cryptos) {
-                objRepartition[test2.coin_id] = {}
-                objRepartition[test2.coin_id].name = test2.symbol
-                objRepartition[test2.coin_id].quantity = test2.total
-                objRepartition[test2.coin_id].value = repartition[test2.coin_id]
-                objRepartition[test2.coin_id].distribution = (100 * repartition[test2.coin_id])/sumValue;
+
+            for (const key in buy) {
+                sumBuy += parseInt(buy[key])
             }
-            console.log(sumValue)
-            portfolio.Distribution = [objRepartition];
-            
+
+            for (const crypto of cryptos) {
+                objRepartition[crypto.coin_id] = {}
+                objRepartition[crypto.coin_id].name = crypto.symbol
+                objRepartition[crypto.coin_id].quantity = crypto.total
+                objRepartition[crypto.coin_id].value = value[crypto.coin_id]
+                objRepartition[crypto.coin_id].distribution = (100 * value[crypto.coin_id])/sumValue;
+            }
+
+            const pnl = sumValue - sumBuy;
+
+            objPerformance.investment = sumBuy;
+            objPerformance.actual_value = sumValue;
+            objPerformance.pnl = pnl;
+
+            portfolio.transactions = objTransactions;
+            portfolio.distribution = [objRepartition];
+            portfolio.performance = [objPerformance];
+
             res.status(200).json(portfolio);
         } catch (error) {
             console.log(error);
