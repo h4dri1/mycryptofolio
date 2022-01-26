d54361073a85aa63520ff490a555f730bfc536d5
h4dri1 <hadrien.vuillaume@gmail.com>
2022-01-26 13:56:32 +0100

    build(back):distribution ok

--- back/app/controllers/transactionController.js
+++ back/app/controllers/transactionController.js
@@ -4,8 +4,11 @@ const fetch = require('cross-fetch');
 module.exports = {
     getPortfolio: async (req, res) => {
         let portfolio = {};
+        let repartition = {};
+        let objRepartition = {};
         let data;
         let searched_coins;
+        let sumValue = 0;
 
         try {
             const transactions = await Transaction.getUserTransaction(req.params.id);
@@ -26,6 +29,24 @@ module.exports = {
                 return res.status(500).json(error.message, true);
             };
             portfolio.Transactions = transactions;
+            console.log(cryptos);
+            console.log(data);
+            for (const test of cryptos) {
+                repartition[test.coin_id] = test.total * data[test.coin_id].usd
+            }
+            console.log(repartition)
+            for (const key in repartition) {
+                sumValue += parseInt(repartition[key])
+            }
+            for (const test2 of cryptos) {
+                objRepartition[test2.coin_id] = {}
+                objRepartition[test2.coin_id].name = test2.symbol
+                objRepartition[test2.coin_id].quantity = test2.total
+                objRepartition[test2.coin_id].value = repartition[test2.coin_id]
+                objRepartition[test2.coin_id].distribution = (100 * repartition[test2.coin_id])/sumValue;
+            }
+            console.log(sumValue)
+            portfolio.Distribution = [objRepartition];
             res.status(200).json(portfolio);
         } catch (error) {
             console.log(error);
