9cecb5885d37fd18777e22cd4f92b2f237e6b73f
Augustin Holguin <augustin.holguin@gmail.com>
2022-01-28 15:42:55 +0100

    feat(portfolio-perf): dynamic data from state

--- front/src/components/Dashboard/Performance/index.js
+++ front/src/components/Dashboard/Performance/index.js
@@ -3,54 +3,67 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 
-const Performance = () => (
-  <Grid container rowSpacing={3}>
-    <Grid item xs={12}>
-      <Typography variant="h6" align="center">Performance</Typography>
-      <Divider sx={{ width: '100%' }} />
-    </Grid>
-    <Grid item xs={12} sm={6}>
-      <Box sx={{
-        display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center', height: '100%',
-      }}
+import { useSelector } from 'react-redux';
+
+const Performance = () => {
+  const { performance } = useSelector((state) => state.portfolio);
+
+  const perfPercentage = (
+    (
+      (performance.actual_value - performance.investment) / performance.investment) * 100
+  ).toFixed(2);
+
+  console.log(perfPercentage);
+
+  return (
+    <Grid container rowSpacing={3}>
+      <Grid item xs={12}>
+        <Typography variant="h6" align="center">Performance</Typography>
+        <Divider sx={{ width: '100%' }} />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Box sx={{
+          display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center', height: '100%',
+        }}
+        >
+          <Typography>Investissement : {`$${Number(performance.investment).toLocaleString()}`}</Typography>
+          <Typography>Valeur actuelle : {`$${Number(performance.actual_value).toLocaleString()}`}</Typography>
+          <Typography>PnL : {`$${Number(performance.pnl).toLocaleString()}`}</Typography>
+        </Box>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        sx={{
+          display: 'flex', justifyContent: 'center', alignItems: 'center',
+        }}
       >
-        <Typography>Investissement : $500</Typography>
-        <Typography>Valeur actuelle : $1,000</Typography>
-        <Typography>PnL : $500</Typography>
-      </Box>
-    </Grid>
-    <Grid
-      item
-      xs={12}
-      sm={6}
-      sx={{
-        display: 'flex', justifyContent: 'center', alignItems: 'center',
-      }}
-    >
-      <Box
-        component="span"
-        sx={[{
-          marginBottom: '15px',
-          width: '70%',
-          maxWidth: '40vw',
-          borderRadius: '50%',
-          border: 'solid 3px #1976D2',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        },
-        {
-          '::after': {
-            display: 'block',
-            paddingBottom: '100%',
-            content: '""',
+        <Box
+          component="span"
+          sx={[{
+            marginBottom: '15px',
+            width: '70%',
+            maxWidth: '40vw',
+            borderRadius: '50%',
+            border: 'solid 3px #1976D2',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
           },
-        },
-        ]}
-      ><Typography variant="h4" color="green">+50%</Typography>
-      </Box>
+          {
+            '::after': {
+              display: 'block',
+              paddingBottom: '100%',
+              content: '""',
+            },
+          },
+          ]}
+        ><Typography variant="h4" color="green">{perfPercentage > 0 ? `+${perfPercentage}%` : `-${perfPercentage}%`}</Typography>
+        </Box>
+      </Grid>
     </Grid>
-  </Grid>
-);
+  );
+};
 
 export default Performance;
