574fd8a4817983e2b9f4fd4e415e701edcc3eff9
h4dri1 <hadrien.vuillaume@gmail.com>
2022-01-30 01:46:59 +0100

    chore(back):add joi on all post route

---
+++ back/app/schemas/transactionSchema.js
@@ -0,0 +1,15 @@
+const Joi = require('joi');
+
+const schema = Joi.object({
+    id: Joi.number(),
+    buy_date: Joi.date().required(),
+    buy: Joi.bool().required(),
+    price: Joi.number().required(),
+    quantity: Joi.number().required(),
+    coin_id: Joi.string().required(),
+    symbol: Joi.string().required(),
+    wallet_id: Joi.number(),
+    crypto_id: Joi.number()
+});
+
+module.exports = schema;
