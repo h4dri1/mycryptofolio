9419624a63cbb09835899ae3b62e90105ff81b19
Etudiant O'clock <g.tannier@gmail.com>
2022-01-31 11:24:37 +0100

    feat(assetsShares):get true data from object distribution

--- front/src/components/Dashboard/AssetsShares/index.js
+++ front/src/components/Dashboard/AssetsShares/index.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 import Container from '@mui/material/Container';
@@ -12,116 +11,59 @@ import Typography from "@mui/material/Typography";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getTransactionsHist } from 'src/actions/portfolio';
-import axios from 'axios';
+import PropTypes from 'prop-types';
 
-const options = {
-    plugins: {
-        legend: {
-            display: false,
-        }
-    },
-};
+export default function AssetsShares({ distribution }) {
 
-const data = {
-    labels: ['Asset 1', 'Asset 2'],
-    datasets: [
-        {
-            label: '% of assets',
-            data: [12, 19],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 2,
+    const labelsList = distribution.map((item) => (
+        item.name
+    ));
+
+    const dataList = distribution.map((item) => (
+        item.distribution
+    ));
+
+    // OPTIONS PIE GRAPH
+    const options = {
+        plugins: {
+            legend: {
+                display: false,
+            }
         },
-    ],
-}
+    };
 
-export default function AssetsShares() {
+    // DATA PIE GRAPH
+    const data = {
 
-    // Dynamisation des éléments du camembert via API
+        //TODO: LABEL à dynamiser
+        labels: labelsList,
+        // labels: ['BTC', 'ETH',...]
+        datasets: [
+            {
+                label: '% of assets',
+                //TODO: DATA à dynamiser
+                data: dataList,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 2,
+            },
+        ],
+    }
 
-    // const [chartData, setChartData] = useState({});
-    // // const [assets, setAssets] = useState([]);
-    // // const [quantityAsset, setQuantityAsset] = useState([]);
-
-    // const chart = () => {
-    //     let asset = [];
-    //     let quantityAsset = [];
-
-    //     axios
-    //         .get('http://...')
-    //         .then(res => {
-    //             console.log('res');
-    //             for (const dataObj of res.data.data) {
-    //                 asset.push(parseInt(dataObj.crypto_id))
-    //                 quantityAsset.push(parseInt(dataObj.quantity))
-    //             }
-
-    //             setChartData({
-    //                 labels: asset, // dynamique variable of name of crypto hold
-    //                 datasets: [
-    //                     {
-    //                         label: '% of assets',
-    //                         data: quantityAsset, // dynamique variable of quantity of crypto hold
-    //                         backgroundColor: [
-    //                             'rgba(255, 99, 132, 0.2)',
-    //                             'rgba(54, 162, 235, 0.2)',
-    //                             'rgba(255, 206, 86, 0.2)',
-    //                             'rgba(75, 192, 192, 0.2)',
-    //                             'rgba(153, 102, 255, 0.2)',
-    //                             'rgba(255, 159, 64, 0.2)',
-    //                         ],
-    //                         borderColor: [
-    //                             'rgba(255, 99, 132, 1)',
-    //                             'rgba(54, 162, 235, 1)',
-    //                             'rgba(255, 206, 86, 1)',
-    //                             'rgba(75, 192, 192, 1)',
-    //                             'rgba(153, 102, 255, 1)',
-    //                             'rgba(255, 159, 64, 1)',
-    //                         ],
-    //                         borderWidth: 2,
-    //                     },
-    //                 ],
-    //             })
-    //         })
-    //         .catch(err => {
-    //             console.log(err);
-    //         });
-    //     console.log(asset, quantityAsset);
-    // }
-
-    // useEffect(() => {
-    //     chart();
-    // }, []);
-
-    // ---------------------------------------------------
-    // TODO: A RETIRER, ne sera plus nécessaire une fois l'API branchée
-
-    const dispatch = useDispatch();
-
-    const { transactionsList } = useSelector((state) => state.portfolio);
-
-    useEffect(() => {
-        dispatch(getTransactionsHist());
-    }, []);
-
-    // ----------------------------------------------------
 
     return (
         <>
@@ -129,28 +71,32 @@ export default function AssetsShares() {
                 <Typography color="primary.main" variant="h6" align="center">Répartition de vos actifs</Typography>
                 <Divider sx={{ width: "100%" }}></Divider>
                 <Container
-                    sx={{ width: '40%', height: '40%', mt: 1 }}>
-                    <Pie data={data} options={options} />
+                    sx={{ width: '40%', height: '40%', mt: 1, }}>
+                    <Pie
+                        data={data} options={options}
+                    />
+                    {/* {jsxLabels}
+                    {jsxData} */}
                 </Container>
                 <Container sx={{
                     display: 'flex', flexDirection: 'column', alignItems: 'center', maxHeight: '30vh', overflowY: 'auto',
                 }}>
-                    <Table stickyHeader sx={{ maxWidth: '100%' }}>
+                    <Table stickyHeader sx={{ maxWidth: '100%', p: '10' }}>
                         <TableHead align="left">
                             <TableRow align='left'>
                                 <TableCell>Devise</TableCell>
                                 <TableCell>Quantité</TableCell>
-                                <TableCell>Total $</TableCell>
-                                <TableCell>répartition</TableCell>
+                                <TableCell>Total$</TableCell>
+                                <TableCell>Répartition</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody align="left">
-                            {transactionsList.map((transaction) => (
-                                <TableRow key={transaction.id}>
-                                    <TableCell>{transaction.crypto_id}</TableCell>
-                                    <TableCell>{transaction.quantity}</TableCell>
-                                    <TableCell>{transaction.price}</TableCell>
-                                    <TableCell>{transaction.rentability}%</TableCell>
+                            {distribution.map((asset, index) => (
+                                <TableRow key={index}>
+                                    <TableCell>{asset.name}</TableCell>
+                                    <TableCell>{asset.quantity}</TableCell>
+                                    <TableCell>{Math.round(asset.value * 100) / 100}</TableCell>
+                                    <TableCell>{asset.distribution.toFixed(0)}%</TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
@@ -158,6 +104,14 @@ export default function AssetsShares() {
                 </Container>
             </Container>
         </>
-    )
+    );
 }
 
+AssetsShares.propTypes = {
+    distribution: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        value: PropTypes.number.isRequired,
+        quantity: PropTypes.number.isRequired,
+        distribution: PropTypes.number.isRequired,
+    })).isRequired,
+};
