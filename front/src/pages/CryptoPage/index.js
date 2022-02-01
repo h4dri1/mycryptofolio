6252d038ce8a4f9ff774a5782f249b19df2f1692
Etudiant O'clock <g.tannier@gmail.com>
2022-02-01 10:24:30 +0100

    fix(develop):fix errors coming from crypto-details

--- front/src/pages/CryptoPage/index.js
+++ front/src/pages/CryptoPage/index.js
@@ -1,21 +1,11 @@
 import TopBanner from 'src/components/TopBanner';
 import Navbar from 'src/components/Navbar';
 import CryptoDetails from 'src/components/CryptoDetails';
-import cryptoDetails from 'src/reducers/cryptoDetails';
-import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 export default function CryptoPage() {
 
-    const { slug } = useParams();
-    const params = useParams();
-    console.log(params);
-
-    // const crypto = useSelector((state) => state.cryptoDetails, slug);
-    // const crypto = useSelector((state) => getDescription(state.cryptoDetails, slug));
     const { crypto } = useSelector((state) => state.cryptoDetails);
-    console.log(crypto);
-    console.log(slug);
 
     return (
         <div className="">
