6b99001c8469ee3ce2f69f472bce4c12def8ef9b
Vincent Mingam <vmingam@gmail.com>
2022-02-04 10:49:10 +0100

    style(logo): adjust logo display

--- front/src/components/Navbar/Logo/index.js
+++ front/src/components/Navbar/Logo/index.js
@@ -2,14 +2,19 @@ import logo from 'src/assets/logo/logo_MyCryptoFolio.png';
 import Avatar from '@mui/material/Avatar';
 
 export default function Logo() {
-    return (
-        <>
-            <Avatar
-                alt="logo MyCryptoFolio"
-                src={logo}
-                sx={{ width: 65, height: 65 }}
-                variant="square"
-            />
-        </>
-    );
+  return (
+    <>
+      <Avatar
+        alt="logo MyCryptoFolio"
+        src={logo}
+        sx={{
+          width: 100,
+          height: 100,
+          margin: '1rem',
+          borderRadius: '1rem',
+        }}
+        variant="square"
+      />
+    </>
+  );
 }
