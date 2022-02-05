0959062034d9ad0091f2a612da059c997c4b06c0
h4dri1 <hadrien.vuillaume@gmail.com>
2022-02-05 15:27:23 +0100

    fix(back):fix refatco bug

--- back/migrations/deploy/view_coin_value.sql
+++ back/migrations/deploy/view_coin_value.sql
@@ -12,8 +12,27 @@ FROM
 	view_transaction
 WHERE 
 	buy=true
-GROUP BY 
-user_id,
+GROUP BY
+	user_id,
+	symbol, 
+	coin_id;
+
+CREATE OR REPLACE VIEW coins_value_wallet AS
+SELECT
+	wallet_id,
+	wallet_label,
+	user_id,
+	symbol AS name,
+	SUM (quantity) AS quantity,
+	SUM (quantity * (SELECT price FROM crypto WHERE crypto.coin_id=view_transaction.coin_id)) AS value
+FROM 
+	view_transaction
+WHERE 
+	buy=true
+GROUP BY
+	wallet_label,
+	wallet_id,
+	user_id,
 	symbol, 
 	coin_id;
 
