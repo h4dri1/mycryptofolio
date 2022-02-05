cd0abcfdbd901071aaf0d24d804acb54dee65e5d
Etudiant O'clock <g.tannier@gmail.com>
2022-02-05 20:34:19 +0100

    feat(graph): add animation

--- front/src/components/CryptoDetails/Graph/index.js
+++ front/src/components/CryptoDetails/Graph/index.js
@@ -11,7 +11,7 @@ import {
 
 import { Line } from 'react-chartjs-2';
 
-import { React, useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 
@@ -27,14 +27,30 @@ ChartJS.register(
 
 // const [displayLine, setDisplayLine] = useState(false);
 
-export default function Graph({ data, chart }) {
+export default function Graph({ chart }) {
 
-    // useEffect(() => {
-    //     setDisplayLine(dataPrices)
-    // }, []);
+    // data for animation
+    const data = [];
+    const data2 = [];
+    let prev = 100;
+    let prev2 = 80;
+    for (let i = 0; i < 1000; i++) {
+        prev += 5 - Math.random() * 10;
+        data.push({ x: i, y: prev });
+        prev2 += 5 - Math.random() * 10;
+        data2.push({ x: i, y: prev2 });
+    }
+
+    // duration of the animation
+    const totalDuration = 100000;
+    const delayBetweenPoints = totalDuration / data.length;
+    const previousY = (ctx) => ctx.index === 0 ? ctx.chart.scales.y.getPixelForValue(100) : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
+
+    // ------------------------------------------
 
 
 
+    // title of graph
     const cryptoName = data.name;
 
     const options = {
@@ -50,6 +66,37 @@ export default function Graph({ data, chart }) {
                 color: '#B5179E'
             },
         },
+        animation: {
+            x: {
+                type: 'number',
+                easing: 'linear',
+                duration: delayBetweenPoints,
+                from: NaN, // the point is initially skipped
+                delay(ctx) {
+                    if (ctx.type !== 'data' || ctx.xStarted) {
+                        return 0;
+                    }
+                    ctx.xStarted = true;
+                    return ctx.index * delayBetweenPoints;
+                }
+            },
+            y: {
+                type: 'number',
+                easing: 'linear',
+                duration: delayBetweenPoints,
+                from: previousY,
+                delay(ctx) {
+                    if (ctx.type !== 'data' || ctx.yStarted) {
+                        return 0;
+                    }
+                    ctx.yStarted = true;
+                    return ctx.index * delayBetweenPoints;
+                }
+            },
+            interaction: {
+                intersect: false
+            }
+        }
 
     };
 
