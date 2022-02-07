b0bd84abb8b5d68b357f6cb553099f494b6d7acf
h4dri1 <hadrien.vuillaume@gmail.com>
2022-02-07 15:15:23 +0100

    test(back):test config webpack

--- front/config/webpack.prod.js
+++ front/config/webpack.prod.js
@@ -70,6 +70,9 @@ module.exports = merge(common, {
     allowedHosts: [
      'mycryptofolio.fr',
      'api.mycryptofolio.fr'
-    ]
+    ],
+    historyApiFallback: true,
+    contentBase: './',
+    hot: true
   }
 });
