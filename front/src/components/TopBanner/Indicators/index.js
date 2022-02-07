1e031cf780fd09be2f1c5e6c699a45a5e30a0acf
Vincent Mingam <vmingam@gmail.com>
2022-02-07 16:30:11 +0100

    style(Home): adjust header

--- front/src/components/TopBanner/Indicators/index.js
+++ front/src/components/TopBanner/Indicators/index.js
@@ -2,101 +2,78 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 
-const Indicators = () => {
-    return (
-        <>
-            <Container
-                disableGutters
-                sx={{
-                    maxWidth: "100%",
-                    display: "flex",
-                    justifyContent: "flex-start",
-                    alignItems: "center",
-                    ml: 1,
-                }}>
-                <Typography
-                    variant="subtitle"
-                    component="div"
-                    sx={{
-                        display: 'inline-flex',
-                        // m: 0.5,
-                        // p: 0,
-                        fontSize: '1rem',
-                        fontWeight: '300',
-                    }}
-                >
-                    Total Market Cap. :
-                </Typography>
+const Indicators = () => (
+  <>
+    <Container
+      disableGutters
+      sx={{
+        maxWidth: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        ml: 1,
+        fontSize: '0.8rem',
+      }}
+    >
+      <Typography
+        variant="subtitle"
+        component="p"
+        ml={2}
+      >
+        Total Market Cap. :
+      </Typography>
 
-                <Typography
-                    sx={{
-                        display: 'inline-flex',
-                        m: 0.8,
-                        // p: 1,
-                        fontSize: '0.8rem',
-                        fontWeight: 'normal',
-                        color: "secondary.main",
-                    }}
-                >
-                    $2,007,030,607,506
-                </Typography>
+      <Typography
+        sx={{
+          m: 0.8,
+          color: 'secondary.main',
+          fontSize: '0.8rem',
+        }}
+      >
+        $2,007,030,607,506
+      </Typography>
 
-                <Typography
+      <Typography
 
-                    variant="subtitle"
-                    component="div"
-                    sx={{
-                        display: 'inline-flex',
-                        ml: 1.5,
-                        // p: 0,
-                        fontSize: '1rem',
-                        fontWeight: '300',
-                    }}
-                >
-                    24h Vol :
-                </Typography>
+        variant="subtitle"
+        component="p"
+        sx={{
+          ml: 5,
+        }}
+      >
+        24h Vol :
+      </Typography>
 
-                <Typography
-                    sx={{
-                        display: 'inline-flex',
-                        ml: 1.5,
-                        // p: 1,
-                        fontSize: '0.8rem',
-                        fontWeight: 'normal',
-                        color: "secondary.main",
-                    }}
-                >
-                    $51,271,689,599
-                </Typography>
+      <Typography
+        sx={{
+          ml: 1.5,
+          color: 'secondary.main',
+          fontSize: '0.8rem',
+        }}
+      >
+        $51,271,689,599
+      </Typography>
 
-                <Typography
-                    variant="subtitle"
-                    component="div"
-                    sx={{
-                        display: 'inline-flex',
-                        ml: 1.5,
-                        // p: 0,
-                        fontSize: '1rem',
-                        fontWeight: '300',
-                    }}
-                >
-                    Dominance :
-                </Typography>
+      <Typography
+        variant="subtitle"
+        component="p"
+        sx={{
+          ml: 5,
+        }}
+      >
+        Dominance :
+      </Typography>
 
-                <Typography
-                    sx={{
-                        display: 'inline-flex',
-                        m: 0.8,
-                        // p: 1,
-                        fontSize: '0.8rem',
-                        fontWeight: 'normal',
-                        color: "secondary.main",
-                    }}
-                >
-                    BTC: 40.2%
-                </Typography>
-            </Container>
-        </>
-    );
-};
+      <Typography
+        sx={{
+          m: 0.8,
+          color: 'secondary.main',
+          fontSize: '0.8rem',
+        }}
+      >
+        BTC: 40.2%
+      </Typography>
+    </Container>
+  </>
+);
+
 export default Indicators;
