da75a5b36e375d3cd45dd1dc0ddb9a58670a3292
h4dri1 <hadrien.vuillaume@gmail.com>
2022-02-07 14:38:00 +0100

    test(back):test config webpack

--- front/config/webpack.prod.js
+++ front/config/webpack.prod.js
@@ -66,4 +66,8 @@ module.exports = merge(common, {
     maxEntrypointSize: 512000,
     maxAssetSize: 512000,
   },
+  devServer: {
+    compress: true,
+    disableHostCheck: true,   // That solved it
+ }  
 });
