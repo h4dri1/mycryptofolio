cc570e10239bfc103f077966b25cc3c7570567ac
Augustin Holguin <augustin.holguin@gmail.com>
2022-02-09 00:21:18 +0100

    fix(nav-slug): trim of whitespace to have a clean slug

--- front/src/middleware/cryptoDetails.js
+++ front/src/middleware/cryptoDetails.js
@@ -14,7 +14,6 @@ const cryptoDetails = (store) => (next) => (action) => {
         url: `/crypto/${action.payload}`,
       })
         .then((res) => {
-          console.log(res.data);
           // fetchCryptoDataSuccess(res.data)
           store.dispatch(fetchCryptoDataSuccess(res.data));
         })
