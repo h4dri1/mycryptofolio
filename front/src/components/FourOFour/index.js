4137db2fc233046928129fefb7eda26c6c7a0850
Augustin Holguin <augustin.holguin@gmail.com>
2022-02-09 16:04:16 +0100

    fix: 404 lighter image

--- front/src/components/FourOFour/index.js
+++ front/src/components/FourOFour/index.js
@@ -3,8 +3,8 @@ import fourOfour from 'src/assets/images/404.jpg';
 import Box from '@mui/material/Box';
 
 const FourOFour = () => (
-  <Box sx={{ marginTop: '130px', display: 'flex', justifyContent: 'center' }}>
-    <img src={fourOfour} style={{ maxWidth: '90vw' }} alt="404" />
+  <Box sx={{ marginTop: '135px', display: 'flex', justifyContent: 'center' }}>
+    <img src={fourOfour} style={{ maxWidth: '90vw', maxHeight: '80vh' }} alt="404" />
   </Box>
 );
 
