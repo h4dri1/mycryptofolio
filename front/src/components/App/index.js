fc271fca053d6f149e11b162e8df04d5414626d4
Augustin Holguin <augustin.holguin@gmail.com>
2022-02-09 15:38:38 +0100

    feat(404): 404 ok

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -3,6 +3,7 @@
 import Home from 'src/pages/Home';
 import Portfolio from 'src/pages/Portfolio';
 import CryptoPage from 'src/pages/CryptoPage';
+import UnknowRoute from 'src/pages/404';
 
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
@@ -69,6 +70,7 @@ const App = () => {
           <Route path="/portfolio" element={<Portfolio />}>
             <Route path="/portfolio/:walletName" element={<Portfolio />} />
           </Route>
+          <Route path="*" element={<UnknowRoute />} />
         </Routes>
         {/* </Paper> */}
       </ThemeProvider>
