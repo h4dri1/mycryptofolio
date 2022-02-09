546f7176438710889876a0e3a597af1dba688530
Etudiant O'clock <g.tannier@gmail.com>
2022-02-09 17:36:46 +0100

    feat(contact):add link onto the whole card

--- front/src/components/Contact/index.js
+++ front/src/components/Contact/index.js
@@ -30,92 +30,93 @@ export default function Contact() {
 
         <Grid display="flex" maxHeight={'100%'} container rowSpacing={{ xs: 1, md: 2 }} justifyContent="space-evenly" className={classes.grid}>
 
-            <Card sx={{ width: 250, display: "flex", flexDirection: "column", alignItems: "center" }}>
-                <Link href="https://github.com/h4dri1">
+            <Link href="https://github.com/h4dri1" underline="none" >
+                <Card sx={{ width: 250, display: "flex", flexDirection: "column", alignItems: "center" }}>
                     <Avatar
                         alt="photo-avatar"
                         src="https://avatars.githubusercontent.com/u/89306281?v=4"
                         sx={{ mt: 3, display: "flex", alignItems: 'center', width: 70, height: 70 }}
                     />
-                </Link>
-                <CardContent>
-                    <Typography variant="h5" component="div" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}  >
-                        Hadrien
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }} >
-                        Lead dev back & Product owner
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    {/* <Button size="small">Share</Button>
+                    <CardContent>
+                        <Typography variant="h5" component="div" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}  >
+                            Hadrien Vuillaume
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }} >
+                            Lead dev back & Product owner
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        {/* <Button size="small">Share</Button>
                     <Button size="small">Learn More</Button> */}
-                </CardActions>
-            </Card>
+                    </CardActions>
+                </Card>
+            </Link>
 
-            <Card sx={{ width: 250, display: "flex", flexDirection: "column", alignItems: "center" }}>
-                <Link href="https://github.com/Augustin-Holguin">
+            <Link href="https://github.com/Augustin-Holguin" underline="none" >
+                <Card sx={{ width: 250, display: "flex", flexDirection: "column", alignItems: "center" }}>
                     <Avatar
                         alt="photo-avatar"
                         src="https://avatars.githubusercontent.com/u/25201511?v=4"
                         sx={{ mt: 3, display: "flex", alignItems: 'center', width: 70, height: 70 }}
                     />
-                </Link>
-                <CardContent>
-                    <Typography variant="h5" component="div" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}  >
-                        Augustin
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }} >
-                        Lead dev front
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    {/* <Button size="small">Share</Button>
+                    <CardContent>
+                        <Typography variant="h5" component="div" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}  >
+                            Augustin Holguín
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }} >
+                            Lead dev front
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        {/* <Button size="small">Share</Button>
                     <Button size="small">Learn More</Button> */}
-                </CardActions>
-            </Card>
+                    </CardActions>
+                </Card>
+            </Link>
 
-            <Card sx={{ width: 250, display: "flex", flexDirection: "column", alignItems: "center" }}>
-                <Link href="https://github.com/vmingam">
+            <Link href="https://github.com/vmingam" underline="none" >
+                <Card sx={{ width: 250, display: "flex", flexDirection: "column", alignItems: "center" }}>
                     <Avatar
                         alt="photo-avatar"
                         src="https://avatars.githubusercontent.com/u/7131900?v=4"
                         sx={{ mt: 3, display: "flex", alignItems: 'center', width: 70, height: 70 }}
                     />
-                </Link>
-                <CardContent>
-                    <Typography variant="h5" component="div" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}  >
-                        Vincent
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }} >
-                        Dev front & Git master
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    {/* <Button size="small">Share</Button>
+                    <CardContent>
+                        <Typography variant="h5" component="div" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}  >
+                            Vincent Mingam
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }} >
+                            Dev front & Git master
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        {/* <Button size="small">Share</Button>
                     <Button size="small">Learn More</Button> */}
-                </CardActions>
-            </Card>
+                    </CardActions>
+                </Card>
+            </Link>
 
-            <Card sx={{ width: 250, display: "flex", flexDirection: "column", alignItems: "center" }}>
-                <Link href="https://github.com/GregoryTannier">
+            <Link href="https://github.com/GregoryTannier" underline="none" >
+                <Card sx={{ width: 250, display: "flex", flexDirection: "column", alignItems: "center" }}>
                     <Avatar
                         alt="photo-avatar"
                         src="https://avatars.githubusercontent.com/u/77966553?v=4"
                         sx={{ mt: 3, display: "flex", alignItems: 'center', width: 70, height: 70 }}
                     />
-                </Link>
-                <CardContent>
-                    <Typography variant="h5" component="div" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}  >
-                        Grégory
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }} >
-                        Dev front & Scrum master
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    {/* <Buttsn size="small">Learn More</Button> */}
-                </CardActions>
-            </Card>
+                    <CardContent>
+                        <Typography variant="h5" component="div" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}  >
+                            Grégory Tannier
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }} >
+                            Dev front & Scrum master
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        {/* <Button size="small">Share</Button>
+                    <Button size="small">Learn More</Button> */}
+                    </CardActions>
+                </Card>
+            </Link>
 
         </Grid >
     );
