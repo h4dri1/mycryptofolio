b7afb60d31ff3fe10991782a810a2e49e1181946
Etudiant O'clock <g.tannier@gmail.com>
2022-02-09 15:34:19 +0100

    feat(contact):set cards

--- front/src/components/Footer/index.js
+++ front/src/components/Footer/index.js
@@ -6,30 +6,48 @@ import {
     Toolbar,
     Typography,
     Link,
+    Container
 } from '@mui/material';
 
 const Footer = () => {
     return (
 
 
-        <Grid sx={{ flexGrow: 1, height: '50px' }}>
+        <Grid display="flex">
 
-            <Toolbar disableGutters
-                sx={{ justifyContent: 'center', bgcolor: "#f6eaf7" }}
+            <Box disableGutters
+                sx={{ justifyContent: 'center', bgcolor: "#f6eaf7", alignItems: "center", display: "flex", width: '100%', height: '50px' }}
             >
-                <Link component={RouterLink} to="/contact">
-                    <Typography
-
-                        variant="subtitle"
-                        noWrap
-                        color="black"
-                        sx={{ display: { xs: 'none', sm: 'flex' }, fontSize: 14, color: 'primary.light' }}
+                <Typography
+                    variant="subtitle"
+                    noWrap
+                    color="black"
+                    sx={{ display: { xs: 'none', sm: 'flex' }, fontSize: 14, color: 'primary.light' }}
+                >
+                    <Link component={RouterLink} to="/contact"
+                        color='primary.light'
                     >
                         Contact
+                    </Link>
+                </Typography>
+                <Box>
+                    <Typography
+                        variant="subtitle"
+                        // noWrap
+                        color="black"
+                        sx={{ fontSize: 14, color: 'primary.light', ml: 15 }}
+                    >
+                        ETH address :
                     </Typography>
-                </Link>
-
-            </Toolbar>
+                    <Typography
+                        variant="subtitle"
+                        // noWrap
+                        color="black"
+                        sx={{ fontSize: 14, color: 'primary.light', ml: 2 }}
+                    >0xDDA2391e6Aab2E8FFcE903196b7899D7795130A1
+                    </Typography>
+                </Box>
+            </Box>
 
         </Grid >
 
