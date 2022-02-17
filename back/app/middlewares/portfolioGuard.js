01bc149b6c07907f1f722de3aa520c3060cb2923
h4dri1 <hadrien.vuillaume@gmail.com>
2022-02-17 16:47:46 +0100

    refacto(back):portfolioGuard + service guard

--- back/app/middlewares/portfolioGuard.js
+++ back/app/middlewares/portfolioGuard.js
@@ -20,7 +20,7 @@ module.exports = {
                 }
             }
             if (res.status === 500) {
-                return
+                console.log('???')
             }
             next();
         } catch (error) {
