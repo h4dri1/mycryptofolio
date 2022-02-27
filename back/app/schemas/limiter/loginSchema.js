18fe4d286605b9be0c9d18a2b2b69eda7d4dff1e
h4dri1 <hadrien.vuillaume@gmail.com>
2022-02-27 01:36:48 +0100

    fix(back):add custom message for rate limite

--- back/app/schemas/limiter/loginSchema.js
+++ back/app/schemas/limiter/loginSchema.js
@@ -1,8 +1,13 @@
+const { RateLimitLogin } = require('../../error');
+
 const loginLimiter = {
     windowMs: 60 * 1000,
     max: 10,
     standardHeaders: true,
     legacyHeaders: false,
+    handler: (req, res, next, options) => {
+        throw new RateLimitLogin();
+    }	
 }
 
 module.exports = loginLimiter
--- back/app/schemas/limiter/signupSchema.js
+++ back/app/schemas/limiter/signupSchema.js
@@ -1,8 +1,13 @@
+const { RateLimitSignup } = require('../../error');
+
 const signupLimiter = {
     windowMs: 60 * 60 * 1000,
     max: 5,
     standardHeaders: true,
     legacyHeaders: false,
+    handler: (req, res, next, options) => {
+        throw new RateLimitSignup();
+    }
 }
 
 module.exports = signupLimiter
