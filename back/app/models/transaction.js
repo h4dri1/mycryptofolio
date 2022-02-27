7fff0c15a92638aaa521b9f73557886761cddc13
h4dri1 <hadrien.vuillaume@gmail.com>
2022-02-27 02:31:33 +0100

    clean(back):clean sql query on models

--- back/app/models/transaction.js
+++ back/app/models/transaction.js
@@ -14,42 +14,90 @@ class Transaction {
     }
 
     static async getUserCrypto(user_id) {
-        const {rows} = await db.query('SELECT wallet_id, wallet_label, coin_id, symbol, AVG(price) as buy_price,\
+        const {rows} = await db.query(
+        'SELECT \
+        wallet_id, wallet_label, coin_id, symbol, \
+        AVG(price) AS buy_price,\
         SUM (quantity) AS total, (AVG(price) * SUM(quantity)) AS investment\
-        FROM view_transaction WHERE user_id=$1 GROUP BY symbol, wallet_id, wallet_label, coin_id;', [user_id]);
+        FROM \
+        view_transaction \
+        WHERE \
+        user_id=$1 \
+        GROUP BY \
+        symbol, wallet_id, wallet_label, coin_id;', 
+        [user_id]
+        );
         return rows.map(row => new Transaction(row));
     }
 
     static async getUserCryptoByWallet(user_id, wallet_id) {
-        const {rows} = await db.query('SELECT wallet_id, wallet_label, coin_id, symbol, AVG(price) as buy_price, SUM(quantity) AS total,\
+        const {rows} = await db.query(
+        'SELECT \
+        wallet_id, wallet_label, coin_id, symbol, \
+        AVG(price) as buy_price, \
+        SUM(quantity) AS total,\
         (AVG(price) * SUM(quantity)) AS investment\
-        FROM view_transaction WHERE user_id=$1 AND wallet_id=$2 GROUP BY wallet_id, wallet_label, symbol, coin_id;', [user_id, wallet_id]);
+        FROM \
+        view_transaction \
+        WHERE \
+        user_id=$1 AND wallet_id=$2 \
+        GROUP BY \
+        wallet_id, wallet_label, symbol, coin_id;',
+        [user_id, wallet_id]
+        );
         return rows.map(row => new Transaction(row));
     }
 
     static async getUserTransaction(user_id) {
-        const {rows} = await db.query('SELECT id, symbol, buy, price, quantity, buy_date \
-        FROM view_transaction WHERE user_id=$1 ORDER BY buy_date DESC;', [user_id]);
+        const {rows} = await db.query(
+        'SELECT \
+        id, symbol, buy, price, quantity, buy_date \
+        FROM \
+        view_transaction \
+        WHERE \
+        user_id=$1 \
+        ORDER BY \
+        buy_date DESC;',
+        [user_id]
+        );
         if (rows) {
             return new Transaction(rows);
         }
     }
 
     static async getUserTransactionByWallet(user_id, wallet_id) {
-        const {rows} = await db.query('SELECT id, symbol, buy, price, quantity, buy_date \
-        FROM view_transaction WHERE user_id=$1 AND wallet_id=$2 ORDER BY buy_date DESC;', [user_id, wallet_id]);
+        const {rows} = await db.query(
+        'SELECT \
+        id, symbol, buy, price, quantity, buy_date \
+        FROM \
+        view_transaction \
+        WHERE \
+        user_id=$1 AND wallet_id=$2 \
+        ORDER BY \
+        buy_date DESC;', 
+        [user_id, wallet_id]
+        );
         return rows.map(row => new Transaction(row));
     }
 
     static async getSumCoinByWalletWithSell(tid) {
-        const {rows} = await db.query('SELECT transaction_id, wallet_id, user_id, coin_id, buy,\
+        const {rows} = await db.query(
+        'SELECT \
+        transaction_id, wallet_id, user_id, coin_id, buy,\
         (SELECT quantity FROM view_transaction WHERE id=$1), \
         (SELECT COUNT (buy = false) FROM view_wallet_user_transaction WHERE buy=false AND \
-                (SELECT coin_id FROM view_wallet_user_transaction WHERE transaction_id=$1)=coin_id) AS sell, \
+        (SELECT coin_id FROM view_wallet_user_transaction WHERE transaction_id=$1)=coin_id) AS sell, \
         (SELECT SUM(quantity) FROM view_transaction WHERE \
-            (SELECT coin_id FROM view_wallet_user_transaction WHERE transaction_id=$1)=coin_id AND wallet_id= \
-                    (SELECT wallet_id FROM view_wallet_user_transaction WHERE transaction_id=$1)) AS total\
-        FROM view_wallet_user_transaction WHERE transaction_id=$1 GROUP BY buy, transaction_id, wallet_id, user_id, coin_id;', [tid]);
+        (SELECT coin_id FROM view_wallet_user_transaction WHERE transaction_id=$1)=coin_id AND wallet_id= \
+        (SELECT wallet_id FROM view_wallet_user_transaction WHERE transaction_id=$1)) AS total\
+        FROM \
+        view_wallet_user_transaction \
+        WHERE \
+        transaction_id=$1 \
+        GROUP BY \
+        buy, transaction_id, wallet_id, user_id, coin_id;', 
+        [tid]
+        );
         return rows.map(row => new Transaction(row));
     }
 
