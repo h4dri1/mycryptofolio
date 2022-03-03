dc1161642733a6f3b05d5ab57ffb9d02baa3b7d7
h4dri1 <hadrien.vuillaume@gmail.com>
2022-03-03 03:20:30 +0100

    add(back):add helmet...

--- back/index.js
+++ back/index.js
@@ -54,6 +54,12 @@ app.set('trust proxy', true)
 
 app.disable('x-powered-by');
 
+app.use(helmet.xssFilter());
+
+app.use(helmet.frameguard({ action: 'deny' }));
+
+app.use(helmet.noSniff());
+
 app.use(cors(corsOptions));
 
 app.use(express.json());
