d91158c87baf1f28dbc82f04840f1be3820cb451
h4dri1 <hadrien.vuillaume@gmail.com>
2022-03-05 01:31:33 +0100

    add(back):re-enabled joi verification for params and add limiter for 3 other routes

--- back/app/schemas/params/getOnePriceSchema.js
+++ back/app/schemas/params/getOnePriceSchema.js
@@ -3,10 +3,10 @@ const Joi = require('joi');
 const schema = Joi.object({
     id: Joi.string().alphanum().required(),
     vs: Joi.string().required(),
-    include_market_cap: Joi.boolean(),
-    include_24hr_vol: Joi.boolean(),
-    include_24hr_change: Joi.boolean(),
-    include_last_updated_at: Joi.boolean()
+    include_market_cap: Joi.string(),
+    include_24hr_vol: Joi.string(),
+    include_24hr_change: Joi.string(),
+    include_last_updated_at: Joi.string()
 });
 
 module.exports = schema;
