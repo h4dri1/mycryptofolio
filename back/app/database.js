7eb310901b5b3b9d059a74b553a73557cb88e4d2
h4dri1 <hadrien.vuillaume@gmail.com>
2022-03-07 19:31:00 +0100

    add(back):startup message with db connection or error

--- back/app/database.js
+++ back/app/database.js
@@ -2,6 +2,14 @@ const { Pool } = require('pg');
 const { createClient } = require('redis');
 
 const redis = createClient();
+redis.on('error', () => {
+    console.log('--> Database Cache Server DISCONNECTED !');
+    process.exit()
+});
+
+redis.on('connect', () => {
+    console.log('--> Database Cache Server Connected');
+});
 redis.connect();
 
 const config = {
@@ -10,4 +18,13 @@ const config = {
 
 const pool = new Pool(config);
 
-module.exports = {pool, redis};
+pool.connect((err) => {
+    if (err) {
+        console.error('--> Database Server DISCONNECTED !');
+        process.exit()
+    } else {
+        console.log('--> Database Server Connected');
+    }
+})
+
+module.exports = { pool, redis };
--- back/index.js
+++ back/index.js
@@ -6,6 +6,8 @@ const { swaggerUi, swaggerSpec } = require('./app/docs/swagger')
 
 const { errorMW } = require('./app/middlewares');
 
+const start_mess = require('./startup');
+
 const router = require('./app/router');
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
@@ -21,17 +23,5 @@ app.use((_, res) => {
 });
 
 app.listen(port, host, () => {
-    console.log(`Run in ${process.env.NODE_ENV} environnement`);
-    if (process.env.NODE_ENV === 'dev') {
-      console.log(`--> Cors option ${process.env.HOSTNAME}`);
-      console.log(`--> Helmet Header Security OFF`);
-      console.log(`--> Trust Proxy OFF`);
-      console.log(`--> Log management verbose : maximum (error stack)`);
-    } else {
-      console.log(`--> Cors option ${process.env.HOSTNAME}`);
-      console.log(`--> Helmet Header Security ON`);
-      console.log(`--> Trust Proxy ON`);
-      console.log(`--> Log management verbose : minimum`);
-    }
-    console.log(`--> Server started on http://${host}:${port}`);
+  start_mess(host, port);
 });
