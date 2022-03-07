daa713b12a8a8a3ac9e5aea4282be2393c383c21
h4dri1 <hadrien.vuillaume@gmail.com>
2022-03-07 03:06:42 +0100

    clean before merge

--- back/app/middlewares/fetchMW.js
+++ back/app/middlewares/fetchMW.js
@@ -8,18 +8,18 @@ module.exports = async (req, res, next) => {
         } else {
             var cryptos = await Transaction.getUserCrypto(req.userId.id);
         }
-            const strCryptos = cryptos.map((crypto) => {
-                return crypto['coin_id']
-            });
-            searched_coins = strCryptos.toString();
-            const data = await service_fetch(`//api.coingecko.com/api/v3/simple/price?ids=${searched_coins}&vs_currencies=usd`);
-            const newData = {}
-            for (const price in data) {        
-                newData.coin_id = price;
-                newData.price = data[price].usd;
-                JSON.stringify(newData)
-                await Crypto.updatePrice(newData);
-            }
+        const strCryptos = cryptos.map((crypto) => {
+            return crypto['coin_id']
+        });
+        searched_coins = strCryptos.toString();
+        const data = await service_fetch(`//api.coingecko.com/api/v3/simple/price?ids=${searched_coins}&vs_currencies=usd`);
+        const newData = {}
+        for (const price in data) {        
+            newData.coin_id = price;
+            newData.price = data[price].usd;
+            JSON.stringify(newData)
+            await Crypto.updatePrice(newData);
+        }
         next();
     } catch (err) {
         next(err);
