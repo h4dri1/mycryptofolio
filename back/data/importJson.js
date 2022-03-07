0a98ddcee322b4444e6067552789024eb74f3960
h4dri1 <hadrien.vuillaume@gmail.com>
2022-03-08 00:28:38 +0100

    fix(back):old db variable replace with pool

--- back/data/importJson.js
+++ back/data/importJson.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 const cryptos = require('./crypto.json');
 
-const db = require('../app/database');
+const { pool } = require('../app/database');
 
 const importData = async () => {
-    await db.query('TRUNCATE TABLE crypto RESTART IDENTITY CASCADE;');
+    await pool.query('TRUNCATE TABLE crypto RESTART IDENTITY CASCADE;');
     for (const crypto of cryptos) {
-        await db.query('INSERT INTO crypto(coin_id, symbol) VALUES($1, $2)', [crypto.id, crypto.symbol]);
+        await pool.query('INSERT INTO crypto(coin_id, symbol) VALUES($1, $2)', [crypto.id, crypto.symbol]);
     }
     db.end();
 };
