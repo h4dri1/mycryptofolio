e7c00873d42cc9e424d9b2b859472a80392e92dc
h4dri1 <hadrien.vuillaume@gmail.com>
2022-03-08 23:53:44 +0100

    add(back):first test ok

--- back/app/database.js
+++ back/app/database.js
@@ -2,6 +2,7 @@ const { Pool } = require('pg');
 const { createClient } = require('redis');
 
 const redis = createClient();
+
 redis.on('error', () => {
     console.log('--> Database Cache Server DISCONNECTED !');
     process.exit()
@@ -10,21 +11,23 @@ redis.on('error', () => {
 redis.on('connect', () => {
     console.log('--> Database Cache Server Connected');
 });
+
 redis.connect();
 
 const config = {
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    allowExitOnIdle: true
 }
 
-const pool = new Pool(config);
+const pool = new Pool(config)
 
-pool.connect((err) => {
-    if (err) {
-        console.error('--> Database Server DISCONNECTED !');
-        process.exit()
-    } else {
-        console.log('--> Database Server Connected');
-    }
-})
+pool.on('error', () => {
+    console.error('--> Database Server DISCONNECTED !');
+    process.exit();
+});
+
+pool.on('connect', () => {
+    console.log('--> Database Server Connected');
+});
 
 module.exports = { pool, redis };
--- back/app/test/routes.test.js
+++ back/app/test/routes.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest')
 const { app } = require('../../server')
+const { redis, pool } = require('../database')
 
 describe('Login Endpoints', () => {
   it('should return a response with status 200', async () => {
@@ -10,5 +11,19 @@ describe('Login Endpoints', () => {
         password: '#0clock$0087',
       })
     expect(res.statusCode).toEqual(200);
+    await redis.disconnect();
+  })
+});
+
+describe('Signup Endpoints', () => {
+  it('should return a response with status 200', async () => {
+    const res = await request(app)
+      .post('/v1/jwt/login')
+      .send({
+        email: 'test@test.fr',
+        password: '#0clock$0087',
+      })
+    expect(res.statusCode).toEqual(200);
+    await redis.disconnect();
   })
 });
