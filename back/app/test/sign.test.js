bc12b8a0c664db6a693accd8c85d82bcc443b294
h4dri1 <hadrien.vuillaume@gmail.com>
2022-03-11 19:43:36 +0100

    add(back):starting add invalid data test

--- back/app/test/sign.test.js
+++ back/app/test/sign.test.js
@@ -3,6 +3,10 @@ const { app } = require('../../server')
 const { redis } = require('../database')
 const { faker } = require('@faker-js/faker');
 
+let token = '';
+let refreshToken = '';
+let refreshTokenBl = '';
+
 beforeAll(async () => {
   const res = await request(app)
   .post('/v1/jwt/login')
@@ -14,7 +18,7 @@ beforeAll(async () => {
   refreshToken = res.body.refreshToken
 });
 
-describe('Login Endpoints', () => {
+describe('Login Endpoints Valid', () => {
   it('should return a response with status 200', async () => {
     const res = await request(app)
       .post('/v1/jwt/login')
@@ -27,12 +31,50 @@ describe('Login Endpoints', () => {
   })
 });
 
-describe('RefreshToken Endpoints', () => {
+describe('Login Endpoints Invalid', () => {
+  it('should return a response with status 400', async () => {
+    const res = await request(app)
+      .post('/v1/jwt/login')
+      .send({
+        email: '',
+        password: '',
+      })
+      .expect("Content-Type", /json/)
+    expect(res.statusCode).toEqual(400);
+  }),
+  it('should return a response with status 401', async () => {
+    const res = await request(app)
+      .post('/v1/jwt/login')
+      .send({
+        email: 'email@email.fr',
+        password: 'password',
+      })
+      .expect("Content-Type", /json/)
+    expect(res.statusCode).toEqual(401);
+  })
+});
+
+describe('RefreshToken Endpoints Valid', () => {
   it('should return a response with status 200', async () => {
     const res = await request(app)
       .get(`/v1/jwt/refresh/${refreshToken}`)
       .expect("Content-Type", /json/)
     expect(res.statusCode).toEqual(200);
+  })
+});
+
+describe('RefreshToken Endpoints Invalid', () => {
+  it('should return a response with status 404', async () => {
+    const res = await request(app)
+      .get(`/v1/jwt/refresh/`)
+      .expect("Content-Type", /json/)
+    expect(res.statusCode).toEqual(404);
+  })
+  it('should return a response with status 500', async () => {
+    const res = await request(app)
+      .get(`/v1/jwt/refresh/sdihjOIJH85sdf`)
+      .expect("Content-Type", /json/)
+    expect(res.statusCode).toEqual(500);
   })
 });
 
