64629cc1d94800dfb3dddbb3926f54ee0b45ba0e
h4dri1 <hadrien.vuillaume@gmail.com>
2022-03-17 02:38:08 +0100

    add(back):add progress bar to importJson to bdd in terminal

--- back/data/importJson.js
+++ back/data/importJson.js
@@ -1,15 +1,24 @@
 require('dotenv').config();
 
+const cliProgress = require('cli-progress');
+
 const cryptos = require('./crypto.json');
 
 const { pool } = require('../app/database');
 
 const importData = async () => {
+    console.log('Import Data into BDD')
+    console.log('Remove present data and refresh id')
+    console.log('Add Data to BDD may take long time')
     await pool.query('TRUNCATE TABLE crypto RESTART IDENTITY CASCADE;');
+    const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    bar1.start(12676, 0);
     for (const crypto of cryptos) {
         await pool.query('INSERT INTO crypto(coin_id, symbol) VALUES($1, $2)', [crypto.id, crypto.symbol]);
+        bar1.increment();
     }
-    console.log('finish')
+    bar1.stop();
+    console.log('Data import finish')
     pool.end();
     process.exit();
 };
