6c547766fb3764b62efff19078e76b291e814440
h4dri1 <hadrien.vuillaume@gmail.com>
2022-05-09 03:08:18 +0200

    chore(back):remove validate query from Joi validateMW

--- back/app/middlewares/validateMW.js
+++ back/app/middlewares/validateMW.js
@@ -1,17 +1,8 @@
-const { FormError, InvalidToken } = require('../error')
+const { FormError } = require('../error')
 
 const validator = {
     validateBody: (schema) => (req, res, next) => {
         const {error} = schema.validate(req.body);
-        if (error) {
-            throw new FormError(error.message)
-        } else {
-            next();
-        }
-    },
-
-    validateQuery: (schema) => (req, res, next) => {
-        const {error} = schema.validate(req.query);
         if (error) {
             throw new FormError(error.message)
         } else {
