619043ca185d39614578038242a740ac801fccd7
h4dri1 <hadrien.vuillaume@gmail.com>
2022-06-19 02:24:05 +0200

    fix(back):fix perist currency change

--- back/app/schemas/body/changeUserSchema.js
+++ back/app/schemas/body/changeUserSchema.js
@@ -3,7 +3,8 @@ const Joi = require('joi');
 const schema = Joi.object({
     id: Joi.number().required(),
     email: Joi.string().email(),
-    nickname: Joi.string().alphanum()
+    nickname: Joi.string().alphanum(),
+    currency: Joi.string().alphanum(),
 });
 
 module.exports = schema;
