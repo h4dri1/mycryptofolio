4b8098dc90d09692554ba505f144d74773102897
h4dri1 <hadrien.vuillaume@gmail.com>
2022-08-02 00:26:45 +0200

    fix(back):fix sendinf email

--- back/app/services/mailer.js
+++ back/app/services/mailer.js
@@ -41,7 +41,7 @@ module.exports = {
             
             let info = await transporter.sendMail({
                 from: '"noresponse@mycryptofolio.fr 👻" <hadri1@hadri1.fr>', // sender address
-                to: req.body.email, // list of receivers
+                to: req.body.email ? req.body.email : req.params.email, // list of receivers
                 subject: "Verify Email", // Subject line
                 text: "Follow this link for validate your email", // plain text body
                 html: `<b>Follow this link for validate your email <a href="https://mycryptofolio.fr/v1/verify/${req.token}">Verify</a> it will be active during 10 minutes</b>`, // html body
