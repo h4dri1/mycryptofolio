94b4c2eeedba970bb8d614f0c00eb3c0ac6bd1b3
h4dri1 <hadrien.vuillaume@gmail.com>
2022-08-23 01:13:18 +0200

    test(front):remove lazy loading home page componements

--- front/src/components/Info/index.js
+++ front/src/components/Info/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/function-component-definition */
-
-import React, { Suspense, lazy } from 'react';
+import * as React from 'react';
 
 import { useEffect } from 'react';
 
@@ -10,21 +9,14 @@ import { getCryptoTrend, getFearGreedIndex } from '../../actions/cryptos';
 
 import { getNFTList, resetNFTQuantity } from '../../actions/nft';
 
-import Loading from '../Loading'
-
 
 import { Grid } from '@mui/material';
 
 import { makeStyles } from '@mui/styles';
-//import TopFlop from './TopFlop'
-//import News from './News'
-//import TopNFT from './TopNFT'
-//import CryptoList from '../CryptoList'
-
-const TopFlop = lazy(() => import('./TopFlop'))
-const News = lazy(() => import('./News'))
-const TopNFT = lazy(() => import('./TopNFT'))
-const CryptoList = lazy(() => import('../CryptoList'))
+import TopFlop from './TopFlop'
+import News from './News'
+import TopNFT from './TopNFT'
+import CryptoList from '../CryptoList'
 
 const useStyles = makeStyles({
     grid: {
@@ -59,6 +51,7 @@ export default function Info() {
     dispatch(getNFTList());
   }, []);
 
+
     return (
 
         <Grid
@@ -66,15 +59,11 @@ export default function Info() {
             className={classes.grid}  
         >
             <Grid item className={classes.gridItem}>
-              <Suspense fallback={<Loading/>}>
                 <TopFlop/>
                 <News/>
                 <TopNFT/>
-              </Suspense>
             </Grid>
-            <Suspense fallback={<Loading/>}>
-              <CryptoList/>
-            </Suspense>
+            <CryptoList/>
         </Grid >
     );
 }
