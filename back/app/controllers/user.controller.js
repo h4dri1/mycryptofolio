9c235a6ecdd2458165466e79ba106ffed0eea9cc
h4dri1 <hadrien.vuillaume@gmail.com>
2022-09-27 16:13:45 +0200

    fix(back):fix http status code on login

--- back/app/controllers/user.controller.js
+++ back/app/controllers/user.controller.js
@@ -17,7 +17,7 @@ module.exports = {
     addUser: async (req, res, next) => {
         try {
             const user = await userService.addUser(req, res, next);
-            res.status(200).json(user);
+            res.status(201).json(user);
         } catch (err) {
             next(err);
         }
