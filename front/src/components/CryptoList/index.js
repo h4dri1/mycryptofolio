f9abded1a7e4b776185c55869de6bc089b3f82d7
h4dri1 <hadrien.vuillaume@gmail.com>
2022-10-19 00:30:06 +0200

    fix(front):close #52

--- front/src/components/CryptoList/index.js
+++ front/src/components/CryptoList/index.js
@@ -232,14 +232,14 @@ const MainContainer = ({noButton}) => {
         onClick={() => setBackdropOpen(!backdropOpen)}
         >
         <Box sx={{display: 'flex'}}>
-          <Fade in={true} sx={{ color: 'white', fontSize: 20, fontWeight: 'bold', marginRight: 2, height: '0px', mt: 11.5, ml: 2}} timeout={{ enter: 3000, exit: 1}}>
+          <Fade in={true} sx={{ color: 'white', fontSize: 20, fontWeight: 'bold', marginRight: 2, height: '0px', mt: {xs: 25, md: 11.5}, ml: 2}} timeout={{ enter: 3000, exit: 1}}>
 
               <Typography >Ajouter d'abors des favoris</Typography>
  
           </Fade>
           <Slide direction="right" in={true} mountOnEnter unmountOnExit timeout={{ enter: 1000, exit: 1}}>
 
-              <ArrowForwardIcon sx={{ fontSize: 50, marginTop: 10.5, marginRight: 1 }} />
+              <ArrowForwardIcon sx={{ fontSize: 50, marginTop: {xs: 29, md: 10.5}, marginRight: {xs: 0, md: 1} }} />
  
           </Slide>
           <MainContainer noButton={true}/>
