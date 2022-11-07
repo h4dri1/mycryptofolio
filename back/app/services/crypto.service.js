331d00a6033d4f802649b3bea26d81f82a2e4a47
h4dri1 <hadrien.vuillaume@gmail.com>
2022-11-07 18:10:05 +0100

    add(back):add explicit error message when no data and throw error for not caching empty array

--- back/app/services/crypto.service.js
+++ back/app/services/crypto.service.js
@@ -1,14 +1,15 @@
 const { Crypto } = require('../models');
 const { fetch } = require('../utils');
-const { NoCryptoFound } = require('../error/error');
+const { NoCryptoFound, AccessDataError } = require('../error/error');
 const { CryptoService } = require('../error/error.services');
 const { OneCryptoObject } = require('../objects');
 
 module.exports = {
   getAllCryptos: async () => {
     try {
-      const AllCryptos = await Crypto.findAll();
-      return AllCryptos;
+      const allCryptos = await Crypto.findAll();
+      if (allCryptos.length === 0) throw new AccessDataError('get cryptos');
+      return allCryptos;
     } catch (err) {
       throw new CryptoService(err);
     }
@@ -17,6 +18,7 @@ module.exports = {
   getTopCryptos: async (req) => {
     try {
       const cryptos = await fetch(`//api.coingecko.com/api/v3/coins/markets?vs_currency=${req.params.vs}&order=market_cap_desc&per_page=${req.params.nb}&page=1&sparkline=false`);
+      if (cryptos.length === 0) throw new AccessDataError('get top cryptos');
       return cryptos.map((row) => new Crypto(row));
     } catch (err) {
       throw new CryptoService(err);
@@ -26,9 +28,8 @@ module.exports = {
   getOneCrypto: async (req) => {
     try {
       const oneCryptoData = await fetch(`//api.coingecko.com/api/v3/coins/${req.params.id}`);
-      if (oneCryptoData.status === 'Not Found') {
-        throw new NoCryptoFound();
-      }
+      if (oneCryptoData.status === 'Not Found') throw new NoCryptoFound();
+      if (oneCryptoData.length === 0) throw new AccessDataError('get one crypto');
       let days = 1;
       if (req.params.nbd) {
         days = req.params.nbd;
@@ -44,6 +45,7 @@ module.exports = {
   getTrendingCryptos: async () => {
     try {
       const trendingCryptosData = await fetch('//api.coingecko.com/api/v3/search/trending');
+      if (trendingCryptosData.coins.length === 0) throw new AccessDataError('trending cryptos');
       return new Crypto(trendingCryptosData);
     } catch (err) {
       throw new CryptoService(err);
@@ -53,6 +55,7 @@ module.exports = {
   getFearAndGreed: async () => {
     try {
       const fearAndGreedData = await fetch('//api.alternative.me/fng/?limit=1');
+      if (fearAndGreedData.data.length === 0) throw new AccessDataError('fear and greed index');
       return new Crypto(fearAndGreedData);
     } catch (err) {
       throw new CryptoService(err);
@@ -62,6 +65,7 @@ module.exports = {
   getGlobalData: async () => {
     try {
       const globalMarketData = await fetch('//api.coingecko.com/api/v3/global');
+      if (globalMarketData.data.length === 0) throw new AccessDataError('global market data');
       return new Crypto(globalMarketData);
     } catch (err) {
       throw new CryptoService(err);
@@ -71,6 +75,7 @@ module.exports = {
   getHistoricalData: async (req) => {
     try {
       const historicalData = await fetch(`//api.coingecko.com/api/v3/coins/${req.params.coinId}/history?date=${req.params.day}-${req.params.month}-${req.params.year}`);
+      if (historicalData.market_data.length === 0) throw new AccessDataError('historical data');
       return new Crypto(historicalData);
     } catch (err) {
       throw new CryptoService(err);
