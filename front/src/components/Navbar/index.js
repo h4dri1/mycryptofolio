256660535bb8340482c1353699f62061391541d0
h4dri1 <hadrien.vuillaume@gmail.com>
2022-11-12 01:39:58 +0100

    fix(front):fix both navbar loading add lazy loading

--- front/src/components/Navbar/index.js
+++ front/src/components/Navbar/index.js
@@ -1,16 +1,25 @@
-import { Box } from '@mui/material';
-import NavbarDesktop from './indexDesktop';
-import NavbarMobile from './indexMobile';
+import { Box, useMediaQuery } from '@mui/material';
+import { Suspense, lazy } from 'react';
+
+const NavbarDesktop = lazy(() => import('./indexDesktop'));
+const NavbarMobile = lazy(() => import('./indexMobile'));
 
 export default function Navbar() {
+  const mobileSize = useMediaQuery('(max-width:600px)');
+
   return (
     <Box>
-      <Box sx={{ display: { xs: 'none', md: 'inline' } }}>
-        <NavbarDesktop />
-      </Box>
-      <Box sx={{ display: { xs: 'inline', md: 'none' } }}>
-        <NavbarMobile />
-      </Box>
+      {mobileSize
+        ? (
+          <Suspense fallback={<></>}>
+            <NavbarMobile />
+          </Suspense>
+        )
+        : (
+          <Suspense fallback={<></>}>
+            <NavbarDesktop />
+          </Suspense>
+        )}
     </Box>
   );
 }
